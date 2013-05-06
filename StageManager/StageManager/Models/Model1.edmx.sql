77b5f6a7668e12a74b2236ad442381939e701108
Bart Schut <bartschut2@hotmail.com>
2013-05-06 21:43:10 +0200

    Eerste model design af
    En boel sintaxfouten in het script maar die compilen alleen verkeerd.

--- StageManager/StageManager/Models/Model1.edmx.sql
+++ StageManager/StageManager/Models/Model1.edmx.sql
@@ -1,237 +1,229 @@
 
--- --------------------------------------------------
--- Entity Designer DDL Script for SQL Server 2005, 2008, and Azure
--- --------------------------------------------------
--- Date Created: 05/06/2013 15:17:21
--- Generated from EDMX file: C:\Users\Eigenaar\Documents\GitHub\StageManager\StageManager\StageManager\Models\Model1.edmx
+
+
+
+-- -----------------------------------------------------------
+-- Entity Designer DDL Script for MySQL Server 4.1 and higher
+-- -----------------------------------------------------------
+-- Date Created: 05/06/2013 19:19:25
+-- Generated from EDMX file: C:\Users\Bart\Documents\GitHub\StageManager\StageManager\StageManager\Models\Model1.edmx
+-- Target version: 3.0.0.0
 -- --------------------------------------------------
 
-SET QUOTED_IDENTIFIER OFF;
-GO
-USE [Stagemanager];
-GO
-IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
-GO
 
 -- --------------------------------------------------
 -- Dropping existing FOREIGN KEY constraints
+-- NOTE: if the constraint does not exist, an ignorable error will be reported.
 -- --------------------------------------------------
 
+--    ALTER TABLE `StageSet` DROP CONSTRAINT `FK_StudentStage`;
+--    ALTER TABLE `StageSet` DROP CONSTRAINT `FK_LeraarStage`;
+--    ALTER TABLE `BedrijfsbegeleiderSet` DROP CONSTRAINT `FK_BedrijfsbegeleiderBedrijf`;
+--    ALTER TABLE `StageSet` DROP CONSTRAINT `FK_BedrijfStage`;
+--    ALTER TABLE `StageSet` DROP CONSTRAINT `FK_BedrijfsbegeleiderStage`;
+--    ALTER TABLE `DocentSet` DROP CONSTRAINT `FK_DocentAlgemeen`;
 
 -- --------------------------------------------------
 -- Dropping existing tables
 -- --------------------------------------------------
-
+SET foreign_key_checks = 0;
+    DROP TABLE IF EXISTS `StudentSet`;
+    DROP TABLE IF EXISTS `DocentSet`;
+    DROP TABLE IF EXISTS `BedrijfsbegeleiderSet`;
+    DROP TABLE IF EXISTS `BedrijfSet`;
+    DROP TABLE IF EXISTS `StageSet`;
+    DROP TABLE IF EXISTS `AlgemeenSet`;
+SET foreign_key_checks = 1;
 
 -- --------------------------------------------------
 -- Creating all tables
 -- --------------------------------------------------
 
--- Creating table 'StudentSet'
-CREATE TABLE [dbo].[StudentSet] (
-    [Studentnummer] int IDENTITY(1,1) NOT NULL,
-    [Name] varchar(1000)  NOT NULL,
-    [Straatnaam_en_nr] varchar(1000)  NOT NULL,
-    [Postcode] varchar(1000)  NOT NULL,
-    [Plaats] varchar(1000)  NOT NULL,
-    [Telefoonnummer] int  NOT NULL,
-    [EC_norm_behaald] bool  NOT NULL
-);
-GO
+CREATE TABLE `StudentSet`(
+	`Studentnummer` int NOT NULL AUTO_INCREMENT UNIQUE, 
+	`Name` longtext NOT NULL, 
+	`Straatnaam_en_nr` longtext NOT NULL, 
+	`Postcode` longtext NOT NULL, 
+	`Plaats` longtext NOT NULL, 
+	`Telefoonnummer` int NOT NULL, 
+	`EC_norm_behaald` bool NOT NULL);
 
--- Creating table 'DocentSet'
-CREATE TABLE [dbo].[DocentSet] (
-    [Leraar_Id] int IDENTITY(1,1) NOT NULL,
-    [Naam] varchar(1000)  NULL,
-    [Straatnaam_en_nr] varchar(1000)  NULL,
-    [Postcode] varchar(1000)  NULL,
-    [Plaats] varchar(1000)  NULL,
-    [Telefoonnummer] varchar(1000)  NULL,
-    [Algemene_InformatieId] int  NOT NULL,
-    [DBU1] smallint  NULL,
-    [DBU2] smallint  NULL,
-    [DBU3] smallint  NULL,
-    [DBU4] smallint  NULL,
-    [AlgemeenId] int  NOT NULL
-);
-GO
+ALTER TABLE `StudentSet` ADD PRIMARY KEY (Studentnummer);
 
--- Creating table 'BedrijfsbegeleiderSet'
-CREATE TABLE [dbo].[BedrijfsbegeleiderSet] (
-    [Id] int IDENTITY(1,1) NOT NULL,
-    [Naam] varchar(1000)  NOT NULL,
-    [Functie] varchar(1000)  NULL,
-    [Telefoonnummer] int  NULL,
-    [Email] varchar(1000)  NULL,
-    [Opleidingsniveau] varchar(1000)  NULL,
-    [Minimale_begeleidingstijd_gegarandeerd] varchar(1000)  NOT NULL,
-    [BedrijfBedrijfs_Id] int  NOT NULL
-);
-GO
 
--- Creating table 'BedrijfSet'
-CREATE TABLE [dbo].[BedrijfSet] (
-    [Bedrijfs_Id] int IDENTITY(1,1) NOT NULL,
-    [Naam] varchar(1000)  NOT NULL,
-    [Straatnaam_en_nummer] varchar(1000)  NOT NULL,
-    [Postcode] varchar(1000)  NOT NULL,
-    [Plaats] varchar(1000)  NOT NULL,
-    [Telefoonnummer] int  NULL,
-    [Website] varchar(1000)  NULL
-);
-GO
 
--- Creating table 'StageSet'
-CREATE TABLE [dbo].[StageSet] (
-    [Start_datum] datetime  NULL,
-    [Eind_datum] datetime  NULL,
-    [Stageopdracht_omschijving] varchar(1000)  NULL,
-    [Tools_en_Vaardigheden] varchar(1000)  NULL,
-    [Risico] varchar(1000)  NULL,
-    [StudentStudentnummer] int  NOT NULL,
-    [LeraarLeraar_Id] int  NOT NULL,
-    [BedrijfBedrijfs_Id] int  NOT NULL,
-    [BedrijfsbegeleiderId] int  NOT NULL,
-    [Stage_Id] int  NOT NULL
-);
-GO
 
--- Creating table 'AlgemeenSet'
-CREATE TABLE [dbo].[AlgemeenSet] (
-    [Id] int IDENTITY(1,1) NOT NULL,
-    [Jaargang] datetime  NOT NULL,
-    [Werk_Uren] smallint  NOT NULL,
-    [Blokken] smallint  NOT NULL
-);
-GO
+CREATE TABLE `DocentSet`(
+	`Leraar_Id` int NOT NULL AUTO_INCREMENT UNIQUE, 
+	`Naam` longtext, 
+	`Straatnaam_en_nr` longtext, 
+	`Postcode` longtext, 
+	`Plaats` longtext, 
+	`Telefoonnummer` longtext, 
+	`Algemene_InformatieId` int NOT NULL, 
+	`DBU1` smallint, 
+	`DBU2` smallint, 
+	`DBU3` smallint, 
+	`DBU4` smallint, 
+	`AlgemeenId` int NOT NULL);
 
--- --------------------------------------------------
--- Creating all PRIMARY KEY constraints
--- --------------------------------------------------
+ALTER TABLE `DocentSet` ADD PRIMARY KEY (Leraar_Id);
 
--- Creating primary key on [Studentnummer] in table 'StudentSet'
-ALTER TABLE [dbo].[StudentSet]
-ADD CONSTRAINT [PK_StudentSet]
-    PRIMARY KEY CLUSTERED ([Studentnummer] ASC);
-GO
 
--- Creating primary key on [Leraar_Id] in table 'DocentSet'
-ALTER TABLE [dbo].[DocentSet]
-ADD CONSTRAINT [PK_DocentSet]
-    PRIMARY KEY CLUSTERED ([Leraar_Id] ASC);
-GO
 
--- Creating primary key on [Id] in table 'BedrijfsbegeleiderSet'
-ALTER TABLE [dbo].[BedrijfsbegeleiderSet]
-ADD CONSTRAINT [PK_BedrijfsbegeleiderSet]
-    PRIMARY KEY CLUSTERED ([Id] ASC);
-GO
 
--- Creating primary key on [Bedrijfs_Id] in table 'BedrijfSet'
-ALTER TABLE [dbo].[BedrijfSet]
-ADD CONSTRAINT [PK_BedrijfSet]
-    PRIMARY KEY CLUSTERED ([Bedrijfs_Id] ASC);
-GO
+CREATE TABLE `BedrijfsbegeleiderSet`(
+	`Id` int NOT NULL AUTO_INCREMENT UNIQUE, 
+	`Naam` longtext NOT NULL, 
+	`Functie` longtext, 
+	`Telefoonnummer` int, 
+	`Email` longtext, 
+	`Opleidingsniveau` longtext, 
+	`Minimale_begeleidingstijd_gegarandeerd` longtext NOT NULL, 
+	`BedrijfBedrijfs_Id` int NOT NULL);
 
--- Creating primary key on [Stage_Id] in table 'StageSet'
-ALTER TABLE [dbo].[StageSet]
-ADD CONSTRAINT [PK_StageSet]
-    PRIMARY KEY CLUSTERED ([Stage_Id] ASC);
-GO
+ALTER TABLE `BedrijfsbegeleiderSet` ADD PRIMARY KEY (Id);
 
--- Creating primary key on [Id] in table 'AlgemeenSet'
-ALTER TABLE [dbo].[AlgemeenSet]
-ADD CONSTRAINT [PK_AlgemeenSet]
-    PRIMARY KEY CLUSTERED ([Id] ASC);
-GO
+
+
+
+CREATE TABLE `BedrijfSet`(
+	`Bedrijfs_Id` int NOT NULL AUTO_INCREMENT UNIQUE, 
+	`Naam` longtext NOT NULL, 
+	`Straatnaam_en_nummer` longtext NOT NULL, 
+	`Postcode` longtext NOT NULL, 
+	`Plaats` longtext NOT NULL, 
+	`Telefoonnummer` int, 
+	`Website` longtext);
+
+ALTER TABLE `BedrijfSet` ADD PRIMARY KEY (Bedrijfs_Id);
+
+
+
+
+CREATE TABLE `StageSet`(
+	`Start_datum` datetime, 
+	`Eind_datum` datetime, 
+	`Stageopdracht_omschijving` longtext, 
+	`Tools_en_Vaardigheden` longtext, 
+	`Risico` longtext, 
+	`StudentStudentnummer` int NOT NULL, 
+	`LeraarLeraar_Id` int NOT NULL, 
+	`BedrijfBedrijfs_Id` int NOT NULL, 
+	`BedrijfsbegeleiderId` int NOT NULL, 
+	`Stage_Id` int NOT NULL);
+
+ALTER TABLE `StageSet` ADD PRIMARY KEY (Stage_Id);
+
+
+
+
+CREATE TABLE `AlgemeenSet`(
+	`Id` int NOT NULL AUTO_INCREMENT UNIQUE, 
+	`Jaargang` datetime NOT NULL, 
+	`Werk_Uren` smallint NOT NULL, 
+	`Blokken` smallint NOT NULL);
+
+ALTER TABLE `AlgemeenSet` ADD PRIMARY KEY (Id);
+
+
+
+
+
 
 -- --------------------------------------------------
 -- Creating all FOREIGN KEY constraints
 -- --------------------------------------------------
 
--- Creating foreign key on [StudentStudentnummer] in table 'StageSet'
-ALTER TABLE [dbo].[StageSet]
-ADD CONSTRAINT [FK_StudentStage]
-    FOREIGN KEY ([StudentStudentnummer])
-    REFERENCES [dbo].[StudentSet]
-        ([Studentnummer])
+-- Creating foreign key on `StudentStudentnummer` in table 'StageSet'
+
+ALTER TABLE `StageSet`
+ADD CONSTRAINT `FK_StudentStage`
+    FOREIGN KEY (`StudentStudentnummer`)
+    REFERENCES `StudentSet`
+        (`Studentnummer`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- Creating non-clustered index for FOREIGN KEY 'FK_StudentStage'
-CREATE INDEX [IX_FK_StudentStage]
-ON [dbo].[StageSet]
-    ([StudentStudentnummer]);
-GO
 
--- Creating foreign key on [LeraarLeraar_Id] in table 'StageSet'
-ALTER TABLE [dbo].[StageSet]
-ADD CONSTRAINT [FK_LeraarStage]
-    FOREIGN KEY ([LeraarLeraar_Id])
-    REFERENCES [dbo].[DocentSet]
-        ([Leraar_Id])
+CREATE INDEX `IX_FK_StudentStage` 
+    ON `StageSet`
+    (`StudentStudentnummer`);
+
+-- Creating foreign key on `LeraarLeraar_Id` in table 'StageSet'
+
+ALTER TABLE `StageSet`
+ADD CONSTRAINT `FK_LeraarStage`
+    FOREIGN KEY (`LeraarLeraar_Id`)
+    REFERENCES `DocentSet`
+        (`Leraar_Id`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- Creating non-clustered index for FOREIGN KEY 'FK_LeraarStage'
-CREATE INDEX [IX_FK_LeraarStage]
-ON [dbo].[StageSet]
-    ([LeraarLeraar_Id]);
-GO
 
--- Creating foreign key on [BedrijfBedrijfs_Id] in table 'BedrijfsbegeleiderSet'
-ALTER TABLE [dbo].[BedrijfsbegeleiderSet]
-ADD CONSTRAINT [FK_BedrijfsbegeleiderBedrijf]
-    FOREIGN KEY ([BedrijfBedrijfs_Id])
-    REFERENCES [dbo].[BedrijfSet]
-        ([Bedrijfs_Id])
+CREATE INDEX `IX_FK_LeraarStage` 
+    ON `StageSet`
+    (`LeraarLeraar_Id`);
+
+-- Creating foreign key on `BedrijfBedrijfs_Id` in table 'BedrijfsbegeleiderSet'
+
+ALTER TABLE `BedrijfsbegeleiderSet`
+ADD CONSTRAINT `FK_BedrijfsbegeleiderBedrijf`
+    FOREIGN KEY (`BedrijfBedrijfs_Id`)
+    REFERENCES `BedrijfSet`
+        (`Bedrijfs_Id`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- Creating non-clustered index for FOREIGN KEY 'FK_BedrijfsbegeleiderBedrijf'
-CREATE INDEX [IX_FK_BedrijfsbegeleiderBedrijf]
-ON [dbo].[BedrijfsbegeleiderSet]
-    ([BedrijfBedrijfs_Id]);
-GO
 
--- Creating foreign key on [BedrijfBedrijfs_Id] in table 'StageSet'
-ALTER TABLE [dbo].[StageSet]
-ADD CONSTRAINT [FK_BedrijfStage]
-    FOREIGN KEY ([BedrijfBedrijfs_Id])
-    REFERENCES [dbo].[BedrijfSet]
-        ([Bedrijfs_Id])
+CREATE INDEX `IX_FK_BedrijfsbegeleiderBedrijf` 
+    ON `BedrijfsbegeleiderSet`
+    (`BedrijfBedrijfs_Id`);
+
+-- Creating foreign key on `BedrijfBedrijfs_Id` in table 'StageSet'
+
+ALTER TABLE `StageSet`
+ADD CONSTRAINT `FK_BedrijfStage`
+    FOREIGN KEY (`BedrijfBedrijfs_Id`)
+    REFERENCES `BedrijfSet`
+        (`Bedrijfs_Id`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- Creating non-clustered index for FOREIGN KEY 'FK_BedrijfStage'
-CREATE INDEX [IX_FK_BedrijfStage]
-ON [dbo].[StageSet]
-    ([BedrijfBedrijfs_Id]);
-GO
 
--- Creating foreign key on [BedrijfsbegeleiderId] in table 'StageSet'
-ALTER TABLE [dbo].[StageSet]
-ADD CONSTRAINT [FK_BedrijfsbegeleiderStage]
-    FOREIGN KEY ([BedrijfsbegeleiderId])
-    REFERENCES [dbo].[BedrijfsbegeleiderSet]
-        ([Id])
+CREATE INDEX `IX_FK_BedrijfStage` 
+    ON `StageSet`
+    (`BedrijfBedrijfs_Id`);
+
+-- Creating foreign key on `BedrijfsbegeleiderId` in table 'StageSet'
+
+ALTER TABLE `StageSet`
+ADD CONSTRAINT `FK_BedrijfsbegeleiderStage`
+    FOREIGN KEY (`BedrijfsbegeleiderId`)
+    REFERENCES `BedrijfsbegeleiderSet`
+        (`Id`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- Creating non-clustered index for FOREIGN KEY 'FK_BedrijfsbegeleiderStage'
-CREATE INDEX [IX_FK_BedrijfsbegeleiderStage]
-ON [dbo].[StageSet]
-    ([BedrijfsbegeleiderId]);
-GO
 
--- Creating foreign key on [AlgemeenId] in table 'DocentSet'
-ALTER TABLE [dbo].[DocentSet]
-ADD CONSTRAINT [FK_DocentAlgemeen]
-    FOREIGN KEY ([AlgemeenId])
-    REFERENCES [dbo].[AlgemeenSet]
-        ([Id])
+CREATE INDEX `IX_FK_BedrijfsbegeleiderStage` 
+    ON `StageSet`
+    (`BedrijfsbegeleiderId`);
+
+-- Creating foreign key on `AlgemeenId` in table 'DocentSet'
+
+ALTER TABLE `DocentSet`
+ADD CONSTRAINT `FK_DocentAlgemeen`
+    FOREIGN KEY (`AlgemeenId`)
+    REFERENCES `AlgemeenSet`
+        (`Id`)
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- Creating non-clustered index for FOREIGN KEY 'FK_DocentAlgemeen'
-CREATE INDEX [IX_FK_DocentAlgemeen]
-ON [dbo].[DocentSet]
-    ([AlgemeenId]);
-GO
+
+CREATE INDEX `IX_FK_DocentAlgemeen` 
+    ON `DocentSet`
+    (`AlgemeenId`);
 
 -- --------------------------------------------------
 -- Script has ended
