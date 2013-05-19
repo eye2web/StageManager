d454c552299d806354862e4f8162e4a28ead5ce0
Sjors Boom <hyppo8@hotmail.com>
2013-05-19 15:11:15 +0200

    Wrapper done zonder methodes

--- StageManager/StageManager/Models/WBedrijf.cs
+++ StageManager/StageManager/Models/WBedrijf.cs
@@ -9,6 +9,99 @@ namespace StageManager.Models
 {
     class WBedrijf:Wrapper<bedrijfset>
     {
+        public int Id
+        {
+            get
+            {
+                return getSet().Bedrijfs_Id;
+            }
+            set
+            {
+                getSet().Bedrijfs_Id = value;
+            }
+        }
+
+        public String Naam
+        {
+            get
+            {
+                return getSet().Naam;
+            }
+            set
+            {
+                getSet().Naam = value;
+            }
+        }
+
+        public int? TelefoonNummer
+        {
+            get
+            {
+                return getSet().Telefoonnummer;
+            }
+            set{
+                getSet().Telefoonnummer = value;
+            }
+        }
+
+        public String Website
+        {
+            get
+            {
+                return getSet().Website;
+            }
+            set
+            {
+                getSet().Website = value;
+            }
+        }
+
+        public List<WAdres> AdresSets
+        {
+            get
+            {
+                List<WAdres> list = new List<WAdres>();
+                for (int i = 0; i < getSet().adressets.Count; i++)
+                {
+                    list.Add(new WAdres(getSet().adressets.ElementAt(i)));
+                }
+                return list;
+            }
+            set
+            {
+                List<adresset> list = new List<adresset>();
+                for (int i = 0; i < value.Count; i++)
+                {
+                    list.Add(value[i].getSet());
+                }
+                getSet().adressets = list;
+                save(getSet());
+            }
+        }
+
+        public List<WBedrijfsBegeleider> BedrijfsbegeleiderSets
+        {
+            get
+            {
+                List<WBedrijfsBegeleider> list = new List<WBedrijfsBegeleider>();
+                for (int i = 0; i < getSet().adressets.Count; i++)
+                {
+                    list.Add(new WBedrijfsBegeleider(getSet().bedrijfsbegeleiderset.ElementAt(i)));
+                }
+                return list;
+            }
+            set
+            {
+                List<bedrijfsbegeleiderset> list = new List<bedrijfsbegeleiderset>();
+                for (int i = 0; i < value.Count; i++)
+                {
+                    list.Add(value[i].getSet());
+                }
+                getSet().bedrijfsbegeleiderset = list;
+                save(getSet());
+            }
+        }
+
         public WBedrijf(bedrijfset set)
             : base(set)
         {
---
+++ StageManager/StageManager/Models/WTool_vaardigheidset.cs
@@ -0,0 +1,75 @@
+﻿using StageManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StageManager.Models
+{
+    class WTool_vaardigheidset:Wrapper<tool_vaardigheidset>
+    {
+        public int Id {
+get{
+return getSet().Id;}
+ set{
+getSet().Id = value;
+}
+}
+        
+        public string Naam {
+get{
+return getSet().Naam;}
+ set{
+getSet().Naam = value;
+}
+}
+
+        public virtual List<WDocent> docentsets {
+            get
+            {
+                List<WDocent> oplSet = new List<WDocent>();
+                for (int i = 0; i < getSet().docentsets.Count; i++)
+                {
+                    oplSet.Add(new WDocent(getSet().docentsets.ElementAt(i)));
+                }
+                return oplSet;
+            }
+            set
+            {
+                List<docentset> list = new List<docentset>();
+                for (int i = 0; i < value.Count; i++)
+                {
+                    list.Add(value[i].getSet());
+                }
+                getSet().docentsets = list;
+                save(getSet());
+            }
+}
+        
+        public virtual List<WStage> stagesets {
+            get
+            {
+                List<WStage> oplSet = new List<WStage>();
+                for (int i = 0; i < getSet().stagesets.Count; i++)
+                {
+                    oplSet.Add(new WStage(getSet().stagesets.ElementAt(i)));
+                }
+                return oplSet;
+            }
+            set
+            {
+                List<stageset> list = new List<stageset>();
+                for (int i = 0; i < value.Count; i++)
+                {
+                    list.Add(value[i].getSet());
+                }
+                getSet().stagesets = list;
+                save(getSet());
+            }
+}
+
+        public WTool_vaardigheidset(tool_vaardigheidset set)
+            : base(set)
+        { }
+    }
+}
