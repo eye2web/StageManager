feec4acb18a8ebc6e2f0a8cb3785d0c4c78d3f2c
Leon van Tuijl <leonvtuyl@hotmail.com>
2013-05-22 12:42:06 +0200

    model update

--- StageManager/Testdata/Testdatav1.sql
+++ StageManager/Testdata/Testdatav1.sql
@@ -107,16 +107,16 @@ UNLOCK TABLES;
 LOCK TABLES `bedrijfsets` WRITE;
 
 DELETE FROM `bedrijfsets` ;
-INSERT INTO `bedrijfsets` VALUES (1, 'Sogeti', '0886600', 'www.sogeti.nl', '4');
-INSERT INTO `bedrijfsets` VALUES (2, 'Thinkwise', '61638', 'www.thinkwisesoftware.com', '3');
-INSERT INTO `bedrijfsets` VALUES (3, 'Smartproginc', '07353333', null, '1');
-INSERT INTO `bedrijfsets` VALUES (4, 'microsoft', null, 'microsoft.com', '2');
-INSERT INTO `bedrijfsets` VALUES (5, 'dell', null, 'dell.nl', '5');
-INSERT INTO `bedrijfsets` VALUES (6, 'ictexpert', null, null, '6');
-INSERT INTO `bedrijfsets` VALUES (7, 'bioware', '432432424', 'bioware.com', '7');
-INSERT INTO `bedrijfsets` VALUES (8, 'apple', null, 'apple.nl', '8');
-INSERT INTO `bedrijfsets` VALUES (9, 'mirabeau', 324344566, 'mirabeau.com', '9');
-INSERT INTO `bedrijfsets` VALUES (10, 'SCH', 5423829843, null, '10');
+INSERT INTO `bedrijfsets` VALUES (1, 'Sogeti', null, 'www.sogeti.nl', 4);
+INSERT INTO `bedrijfsets` VALUES (2, 'Thinkwise', 61638, 'www.thinkwisesoftware.com', 3);
+INSERT INTO `bedrijfsets` VALUES (3, 'Smartproginc', 07353333, null, 1);
+INSERT INTO `bedrijfsets` VALUES (4, 'microsoft', null, 'microsoft.com', 2);
+INSERT INTO `bedrijfsets` VALUES (5, 'dell', null, 'dell.nl', 5);
+INSERT INTO `bedrijfsets` VALUES (6, 'ictexpert', null, null, 6);
+INSERT INTO `bedrijfsets` VALUES (7, 'bioware', 432432424, 'bioware.com', 7);
+INSERT INTO `bedrijfsets` VALUES (8, 'apple', null, 'apple.nl', 8);
+INSERT INTO `bedrijfsets` VALUES (9, 'mirabeau', 324344566, 'mirabeau.com',  9);
+INSERT INTO `bedrijfsets` VALUES (10, 'SCH', 5423829843, null, 10);
 
 UNLOCK TABLES;
 
@@ -125,7 +125,7 @@ UNLOCK TABLES;
 LOCK TABLES `persoonsets` WRITE;
 
 DELETE FROM `persoonsets` ;
-INSERT INTO `persoonsets` VALUES (1, 'Leon', 'van Tuijl', 'lpjtuijl@avans.nl', 06253464532);
+INSERT INTO `persoonsets` VALUES (1, 'Leon', 'van Tuijl', 'lpjtuijl@avans.nl', '06253464532');
 INSERT INTO `persoonsets` VALUES (2, 'Bart', 'Schut', 'bschut1@avans.nl', null);
 INSERT INTO `persoonsets` VALUES (3, 'Henk', 'de Vries', 'henkv@hotmail.com', null);
 INSERT INTO `persoonsets` VALUES (4, 'Pieter', 'Hogendijk', 'pietertje@gmail.com', null);
@@ -135,7 +135,7 @@ INSERT INTO `persoonsets` VALUES (7, 'Robert', 'bovenkamp', 'bovenkamp@gmail.com
 INSERT INTO `persoonsets` VALUES (8, 'Gerard', 'Eikendoorn', 'programmer123@hotmail.com', null);
 INSERT INTO `persoonsets` VALUES (9, 'Theo', 'Maasdonk', 'Theomasen@home.nl', null);
 INSERT INTO `persoonsets` VALUES (10, 'Katinka', 'Jansen', 'kjansen@avans.nl', null);
-
+INSERT INTO `persoonsets` VALUES (11, 'Eric', 'Apeldoorn', 'mgnrn@home.nl', '0614235243');
 
 UNLOCK TABLES;
 
@@ -154,6 +154,7 @@ INSERT INTO `webkeysets` VALUES (8, '00008', 'actief');
 INSERT INTO `webkeysets` VALUES (9, '00009', 'actief');
 INSERT INTO `webkeysets` VALUES (10, '00010', 'actief');
 
+
 UNLOCK TABLES;
 
 /*docenten*/
@@ -161,8 +162,9 @@ UNLOCK TABLES;
 LOCK TABLES `docentsets` WRITE;
 
 DELETE FROM `docentsets` ;
-INSERT INTO `docentsets` VALUES (1, 0.65, 1, 1, 0.23);
-INSERT INTO `docentsets` VALUES (1, '', '', '', '');
+INSERT INTO `docentsets` VALUES (5, 0.65, 1, 1, 0.23, 1, 4,1, 'Openbaar vervoer');
+INSERT INTO `docentsets` VALUES (6, 0.65, 1, 1, 1, 1, 7,2, 'Auto');
+INSERT INTO `docentsets` VALUES (7, 1, 1, 1, 1, 7, 3,3, 'Fiets');
 
 UNLOCK TABLES;
 
@@ -171,7 +173,11 @@ UNLOCK TABLES;
 LOCK TABLES `studentsets` WRITE;
 
 DELETE FROM `studentsets` ;
-INSERT INTO `studentsets` VALUES (1, '', '', '', '');
+INSERT INTO `studentsets` VALUES (205170, 1, 2, 1, 4);
+INSERT INTO `studentsets` VALUES (205120, 0, 2, 2, 5);
+INSERT INTO `studentsets` VALUES (206120, 0, 1, 3, 6);
+INSERT INTO `studentsets` VALUES (209199, 0, 10, 4, 7);
+
 
 UNLOCK TABLES;
 
@@ -180,42 +186,91 @@ UNLOCK TABLES;
 LOCK TABLES `bedrijfsbegeleidersets` WRITE;
 
 DELETE FROM `bedrijfsbegeleidersets` ;
-INSERT INTO `bedrijfsbegeleidersets` VALUES (1, '', '', '', '');
-
+INSERT INTO `bedrijfsbegeleidersets` VALUES ('Software Engineer', 'HBO', 1, 8, 2);
+INSERT INTO `bedrijfsbegeleidersets` VALUES ('Architect', 'WO', 1, 9, 3);
+INSERT INTO `bedrijfsbegeleidersets` VALUES ('Architect', 'HBO', 0, 11, 1);
 UNLOCK TABLES;
 
+/*coordinatoren*/
+LOCK TABLES `coordinator` WRITE;
 
+DELETE FROM `coordinator` ;
+INSERT INTO `coordinator` VALUES (10, 'katinka', 'avans1', 1);
+
+UNLOCK TABLES;
 
 /*stage*/
 LOCK TABLES `stagesets` WRITE;
 
 DELETE FROM `stagesets` ;
-INSERT INTO `stagesets` VALUES (1, '', '', '', '');
+INSERT INTO `stagesets` VALUES (1, '2014-10-20', '2015-3-20', 'In mijn opdracht zal ik een website maken via php. Dit zal in opdracht zijn van het bedrijf en ik zal zelf een klant interviewen.', null, 3, null);
+INSERT INTO `stagesets` VALUES (2, '2016-8-25', '2017-1-15', 'C# project', 8, 2, null);
+INSERT INTO `stagesets` VALUES (3, '2016-8-25', '2017-1-15', 'Afstudeerstage waarbij een management systeem gemaakt moet worden', 8, 4, 7);
+INSERT INTO `stagesets` VALUES (4, '2012-8-25', '2013-1-15', 'Afstudeerstage java', 8, 1, null);
 
 UNLOCK TABLES;
 
+LOCK TABLES `eindstagesets` WRITE;
 
+DELETE FROM `eindstagesets` ;
+INSERT INTO `eindstagesets` VALUES (3, 6);
+INSERT INTO `eindstagesets` VALUES (4, null);
+
+UNLOCK TABLES;
 
 
 /*koppel*/
 
-LOCK TABLES `tool_vaardigheiddocentset` WRITE;
+LOCK TABLES `kennisgebieddocentset` WRITE;
 
-DELETE FROM `tool_vaardigheiddocentset` ;
-INSERT INTO `tool_vaardigheiddocentset` VALUES (1, '', '', '', '');
+DELETE FROM `kennisgebieddocentset` ;
+INSERT INTO `kennisgebieddocentset` VALUES (5, 4);
+INSERT INTO `kennisgebieddocentset` VALUES (5, 5);
+INSERT INTO `kennisgebieddocentset` VALUES (5, 10);
+INSERT INTO `kennisgebieddocentset` VALUES (5, 7);
+INSERT INTO `kennisgebieddocentset` VALUES (5, 3);
+INSERT INTO `kennisgebieddocentset` VALUES (7, 1);
+INSERT INTO `kennisgebieddocentset` VALUES (7, 2);
+INSERT INTO `kennisgebieddocentset` VALUES (7, 3);
+INSERT INTO `kennisgebieddocentset` VALUES (7, 10);
+INSERT INTO `kennisgebieddocentset` VALUES (6, 6);
+INSERT INTO `kennisgebieddocentset` VALUES (6, 7);
+INSERT INTO `kennisgebieddocentset` VALUES (6, 9);
 
 UNLOCK TABLES;
 
-LOCK TABLES `tool_vaardigheidstageset` WRITE;
+LOCK TABLES `kennisgebiedstageset` WRITE;
 
-DELETE FROM `tool_vaardigheidstageset` ;
-INSERT INTO `tool_vaardigheidstageset` VALUES (1, '', '', '', '');
+DELETE FROM `kennisgebiedstageset` ;
+INSERT INTO `kennisgebiedstageset` VALUES (1, 1);
+INSERT INTO `kennisgebiedstageset` VALUES (1, 2);
+INSERT INTO `kennisgebiedstageset` VALUES (1, 6);
+INSERT INTO `kennisgebiedstageset` VALUES (1, 7);
+INSERT INTO `kennisgebiedstageset` VALUES (1, 9);
+INSERT INTO `kennisgebiedstageset` VALUES (2, 3);
+INSERT INTO `kennisgebiedstageset` VALUES (2, 5);
+INSERT INTO `kennisgebiedstageset` VALUES (2, 10);
+INSERT INTO `kennisgebiedstageset` VALUES (3, 1);
+INSERT INTO `kennisgebiedstageset` VALUES (3, 5);
+INSERT INTO `kennisgebiedstageset` VALUES (3, 10);
+INSERT INTO `kennisgebiedstageset` VALUES (4, 7);
+INSERT INTO `kennisgebiedstageset` VALUES (4, 8);
+INSERT INTO `kennisgebiedstageset` VALUES (4, 4);
+INSERT INTO `kennisgebiedstageset` VALUES (4, 5);
 
 UNLOCK TABLES;
 
 LOCK TABLES `opleidingdocentset` WRITE;
 
 DELETE FROM `opleidingdocentset` ;
-INSERT INTO `opleidingdocentset` VALUES (1, '', '', '', '');
+INSERT INTO `opleidingdocentset` VALUES (5, 1);
+INSERT INTO `opleidingdocentset` VALUES (5, 2);
+INSERT INTO `opleidingdocentset` VALUES (7, 1);
+INSERT INTO `opleidingdocentset` VALUES (7, 2);
+INSERT INTO `opleidingdocentset` VALUES (7, 3);
+INSERT INTO `opleidingdocentset` VALUES (6, 4);
+INSERT INTO `opleidingdocentset` VALUES (6, 5);
+INSERT INTO `opleidingdocentset` VALUES (6, 6);
+INSERT INTO `opleidingdocentset` VALUES (6, 7);
 
 UNLOCK TABLES;
