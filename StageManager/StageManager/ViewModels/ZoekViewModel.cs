df44e84338a14246f771993305e6c860f4e14a2f
Sjors Boom <hyppo8@hotmail.com>
2013-05-28 16:26:32 +0200

    commit

--- StageManager/StageManager/ViewModels/ZoekViewModel.cs
+++ StageManager/StageManager/ViewModels/ZoekViewModel.cs
@@ -18,21 +18,35 @@ namespace StageManager.ViewModels
             set
             {
                 searchString = value;
+                searchStudent();
                 OnPropertyChanged("SearchString");
-                GridContents = (from student in WStored.SearchStudentSet(SearchString, "")
-                                       select (Object)new
-                                       {
-                                           StudentNummer = student.Studentnummer,
-                                           Voornaam = student.Voornaam,
-                                           Achternaam = student.Achternaam,
-                                           Opleiding = student.Opleidingset.Naam,
-                                           EC_Norm_Behaald = student.EC_norm_behaald
-
-                                       }).ToList();
             }
         }
-        private List<Object> gridContents;
 
+        private List<String> opleidingStack;
+        public List<String> OpleidingStack
+        {
+            get { return opleidingStack; }
+            set
+            {
+                opleidingStack = value;
+                OnPropertyChanged("OpleidingStack");
+            }
+        }             
+
+        private String searchOpleiding;
+        public String SearchOpleiding
+        {
+            get { return searchOpleiding; }
+            set
+            {
+                searchOpleiding = value;
+                searchStudent();
+                OnPropertyChanged("SearchOpleiding");
+            }
+        }
+
+        private List<Object> gridContents;
         public List<object> GridContents
         {
             get
@@ -50,6 +64,32 @@ namespace StageManager.ViewModels
         {
             SearchString = "";
             searchString = "Trefwoord(en)";
+            OpleidingStack = (from opleiding in WStored.SearchOpleidingSet() select opleiding.Naam).ToList();
+
+        }
+
+        public void searchStudent()
+        {
+            List<Object> list = new List<object>();
+            list = (from student in WStored.SearchStudentSet(searchString, searchOpleiding)
+                    select (Object)new
+                    {
+                        StudentNummer = student.Studentnummer,
+                        Voornaam = student.Voornaam,
+                        Achternaam = student.Achternaam,
+                        Opleiding = student.Opleidingset.Naam,
+                        EC_Norm_Behaald = student.EC_norm_behaald
+
+                    }).ToList();
+            if (list.Count == 0)
+            {
+                list.Add((Object)new
+                {
+                    Error = "No occurrences found!"
+                });
+
+            }
+                GridContents = list;
         }
     }
 }
