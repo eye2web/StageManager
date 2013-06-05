16aaaa52970527525961468c7dad15890237f3fa
Sjors Boom <hyppo8@hotmail.com>
2013-06-06 00:11:23 +0200

    Mailing 0.5

---
+++ StageManager/StageManager/Services/Mailer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace StageManager.Services
+{
+    class Mailer
+    {
+        private static LinkedList<MailMessage> mails;
+        private static SmtpClient smtp = new SmtpClient();
+        private static bool _init;
+        private static bool isSending;
+
+        public static bool IsSending
+        {
+            get { return Mailer.isSending; }
+            set { Mailer.isSending = value; }
+        }
+
+        private static void init()
+        {
+            if (!_init)
+            {
+                mails = new LinkedList<MailMessage>();
+                smtp = new SmtpClient("smtp.gmail.com", 587);
+                smtp.SendCompleted += next;
+                smtp.Credentials = new NetworkCredential("min08.stagemanager@gmail.com", "Stagemanager");
+
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.EnableSsl = true;
+                isSending = false;
+                _init = true;
+            }
+        }
+
+        private static void next(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                if (mails.Count > 0)
+                {
+                    smtp.SendAsync(mails.Last.Value, null);
+                    isSending = true;
+                    mails.RemoveLast();
+                }
+                else
+                {
+                    isSending = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+            }
+        }
+
+        public static void Send(String to, String body, String Subject, IDictionary replacements = null)
+        {
+            init();
+            char[] c = { ',', ';', ' ', ':', '\t' };
+            String[] To = to.Split(c);
+            for (int i = 0; i < to.Length; i++)
+            {
+                MailDefinition m = new MailDefinition();
+                if (!replacements.Contains("\n"))
+                {
+                    replacements.Add("\n", "<br/>");
+                }
+                m.IsBodyHtml = true;
+                m.Subject = Subject;
+                m.From = "min08.stagemanager@gmail.com";
+                mails.AddLast(m.CreateMailMessage(to, replacements, body, new System.Web.UI.Control()));
+            }
+            if (!isSending)
+            {
+                next(null, null);
+            }
+        }
+    }
+}
---
+++ StageManager/StageManager/ViewModels/MailViewModel.cs
@@ -0,0 +1,34 @@
+﻿using StageManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StageManager.ViewModels
+{
+    class MailViewModel
+    {
+        public String To { get; set; }
+        public String Message { get; set; }
+        public String Subject { get; set; }
+
+        public MailViewModel()
+        {
+            Message = "Beste student,\n\n" +
+                "Met deze mail vragen wij vriendelijk om je gegevens in te vullen in dit web formulier\n" +
+                "%webkey%\n\n\n" +
+                "Met vriendelijke groeten,\n" +
+                "Katinka Janssen\n" +
+                "Stagecoördinator AI&I\n" +
+                "Avans Hogeschool 's-Hertogenbosch";
+        }
+        public void Send()
+        {
+            ListDictionary replacements = new ListDictionary();
+            replacements.Add("%webkey%", "<a href='webkey'>REPLACED</a>");
+            Mailer.Send(To, Message, Subject, replacements);
+        }
+    }
+}
