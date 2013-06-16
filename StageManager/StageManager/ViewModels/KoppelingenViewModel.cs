a90579c4ef10991662f11e91240cc3934a77782f
Bart Schut <bartschut2@hotmail.com>
2013-06-16 12:51:04 +0200

    Gemaakte Koppelingen overzicht

---
+++ StageManager/StageManager/ViewModels/KoppelingenViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StageManager.Models;
+using StageManager.Views;
+using StageManager.Controllers;
+using Caliburn.Micro;
+using StageManager.Services;
+
+namespace StageManager.ViewModels
+{
+    class KoppelingenViewModel : PropertyChanged
+    {        
+        public KoppelingenViewModel(MainViewModel main)
+        {
+        }
+    }
+}
